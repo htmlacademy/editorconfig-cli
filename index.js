6b728b4d7fbdc50a197c91c65ca69eb0f11fd268
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2017-03-13 22:50:00 +0300

    added eslint validation

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 #!/usr/bin/env node
-const Validator = require('lintspaces');
-const types = require('lintspaces/lib/constants/types');
-const path = require('path');
-const fs = require('fs');
-const program = require('commander');
-const glob = require('glob-fs');
-const util = require('util');
+const Validator = require(`lintspaces`);
+const types = require(`lintspaces/lib/constants/types`);
+const path = require(`path`);
+const fs = require(`fs`);
+const program = require(`commander`);
+const glob = require(`glob-fs`);
+const util = require(`util`);
 // Colors https://www.npmjs.com/package/colors
-require('colors');
+require(`colors`);
 
-const VERBOSE_KEYS = ['-v', '--verbose'];
-const VERBOSE = process.argv.find((element) => {
-    return VERBOSE_KEYS.indexOf(element) >= 0;
-  }) !== undefined;
-const DEFAULT_EDITORCONFIG_NAME = '.editorconfig';
-const JSON_CONFIG_PROPERTY_NAME = 'editorconfig-cli';
-const DEFAULT_JSON_FILENAME = 'package.json';
+const VERBOSE_KEYS = [`-v`, `--verbose`];
+const VERBOSE = !!(process.argv.find((element) => {
+  return VERBOSE_KEYS.indexOf(element) >= 0;
+}));
+const DEFAULT_EDITORCONFIG_NAME = `.editorconfig`;
+const JSON_CONFIG_PROPERTY_NAME = `editorconfig-cli`;
+const DEFAULT_JSON_FILENAME = `package.json`;
 
 
-//Iterate over object props
-Object.prototype[Symbol.iterator] = function*() {
+// Iterate over object props
+Object.prototype[Symbol.iterator] = function* () {
   for (let key of Object.keys(this)) {
-    yield([key, this[key]])
+    yield ([key, this[key]]);
   }
 };
 
@@ -62,16 +62,16 @@ let collect = (value, memo) => {
 };
 
 program
-  .usage('[options] \<file ... or \'glob\'\>')
-  .option('-e, --editorconfig <file>',
-    'pass .editorconfig (by default it will look in \'./.editorconfig\')',
+  .usage(`[options] \<file ... or 'glob'\>`)
+  .option(`-e, --editorconfig <file>`,
+    `pass .editorconfig (by default it will look in './.editorconfig')`,
     checkEditorConfig)
-  .option('-i, --ignores <profile-name or regexp>', 'ignoring profiles. Like (\'js-comments\'' +
-    '|\'java-comments\'|\'xml-comments\'|\'html-comments\'|...). Defaults are \'js-comments\'|\'html-comments\'',
-    ['js-comments', 'html-comments'])
-  .option('-j, --json <file>', 'load GLOBs from JSON file. If no input passed, then it tries to find array in package.json')
-  .option('-x, --exclude <regexp>', 'exclude files by pattern. Default \'normalize.*\'', collect, ['/normalize.*'])
-  .option(VERBOSE_KEYS.join(', '), 'verbose output')
+  .option(`-i, --ignores <profile-name or regexp>`, `ignoring profiles. Like ('js-comments'` +
+    `|'java-comments'|'xml-comments'|'html-comments'|...). Defaults are 'js-comments'|'html-comments'`,
+    [`js-comments`, `html-comments`])
+  .option(`-j, --json <file>`, `load GLOBs from JSON file. If no input passed, then it tries to find array in package.json`)
+  .option(`-x, --exclude <regexp>`, `exclude files by pattern. Default 'normalize.*'`, collect, [`/normalize.*`])
+  .option(VERBOSE_KEYS.join(`, `), `verbose output`)
   .parse(process.argv);
 
 let settings = {
@@ -86,15 +86,15 @@ log.debug(`Passed args: '${program.args}'`);
 
 let exitCode = 0;
 
-process.on('beforeExit', () => {
+process.on(`beforeExit`, () => {
   process.exit(exitCode);
 });
 
 let printReport = function (report) {
   for (let [filename, info] of report) {
-    log.error(util.format('\nFile: %s', filename).red.underline);
+    log.error(util.format(`\nFile: %s`, filename).red.underline);
 
-    for (let [index, line] of info) {
+    for (let [, line] of info) {
       for (let err of line) {
         let type = err.type;
 
@@ -105,19 +105,19 @@ let printReport = function (report) {
           exitCode = 1;
         }
 
-        log.error(util.format('Line: %s %s [%s]', err.line, err.message, type));
+        log.error(util.format(`Line: %s %s [%s]`, err.line, err.message, type));
       }
     }
   }
 
 };
 
-let validate = (path) => {
-  fs.lstat(path, (err, stat) => {
+let validate = (filePath) => {
+  fs.lstat(filePath, (err, stat) => {
     if (!(stat.isDirectory())) {
-      log.debug(`Validating '${path}'...`);
+      log.debug(`Validating '${filePath}'...`);
       let validator = new Validator(settings);
-      validator.validate(path);
+      validator.validate(filePath);
       printReport(validator.getInvalidFiles());
     }
   });
@@ -135,7 +135,7 @@ let onFile = function (file) {
 
     let excluded = exclude.test(file.path);
     if (excluded) {
-      log.info(`File: ${file.path} [${'excluded'.green}]`);
+      log.info(`File: ${file.path} [${`excluded`.green}]`);
     }
 
     return excluded;
@@ -148,13 +148,13 @@ let onFile = function (file) {
 
 let processInput = function (args) {
   for (let it of args) {
-    var resolved = resolve(it);
+    let resolved = resolve(it);
     if (resolved) {
       validate(resolved);
     } else {
       log.debug(`Calling GLOB: ${it}`);
       let myGlob = glob({gitignore: true});
-      myGlob.readdirStream(it).on('data', onFile);
+      myGlob.readdirStream(it).on(`data`, onFile);
     }
   }
 };
@@ -162,13 +162,15 @@ let processInput = function (args) {
 let args = Array.isArray(program.args) ? program.args : [program.args];
 if (args.length === 0) {
   let found = resolve(settings.json);
-  fs.readFile(found, 'utf8', (err, data) => {
+  fs.readFile(found, `utf8`, (err, data) => {
     log.debug(`Reading GLOBs from file: '${found}...`);
     try {
-      if (err) throw err;
-      var globs = JSON.parse(data)[JSON_CONFIG_PROPERTY_NAME];
+      if (err) {
+        throw err;
+      }
+      let globs = JSON.parse(data)[JSON_CONFIG_PROPERTY_NAME];
       if (!globs || globs.length === 0) {
-        log.info('Nothing to do =(');
+        log.info(`Nothing to do =(`);
         program.help();
       } else {
         log.info(`Loaded GLOBs from '${found}': ${globs}`);
@@ -177,7 +179,7 @@ if (args.length === 0) {
     } catch (e) {
       log.error(`Failed to read JSON file: ${e}`.red);
     }
-  })
+  });
 } else {
   processInput(args);
 }
