9bdce06da455faba8fe3a9c9d5d02cdff335b177
firefoxic <firefoxic.dev@gmail.com>
2022-09-25 11:06:19 +0300

    Fix lint issues

--- index.js
+++ index.js
@@ -66,13 +66,8 @@ const collect = (value, memo) => {
 
 program
   .usage(`[options] \<file ... or 'glob'\>`)
-  .option(`-e, --editorconfig <file>`,
-    `pass .editorconfig (by default it will look in './.editorconfig').\
-  !Warning! absolute paths are not supported or will break on Windows OS`,
-    checkEditorConfig)
-  .option(`-i, --ignores <profile-name or regexp>`, `ignoring profiles. Like ('js-comments'` +
-    `|'java-comments'|'xml-comments'|'html-comments'|...). Defaults are 'js-comments'|'html-comments'`,
-    [`js-comments`, `html-comments`])
+  .option(`-e, --editorconfig <file>`, `pass .editorconfig (by default it will look in './.editorconfig').\n!Warning! absolute paths are not supported or will break on Windows OS`, checkEditorConfig)
+  .option(`-i, --ignores <profile-name or regexp>`, `ignoring profiles. Like ('js-comments'|'java-comments'|'xml-comments'|'html-comments'|...). Defaults are 'js-comments'|'html-comments'`, [`js-comments`, `html-comments`])
   .option(`-j, --json <file>`, `load GLOBs from JSON file. If no input passed, then it tries to find array in package.json`)
   .option(`-x, --exclude <regexp>`, `exclude files by pattern. Default 'normalize.*'`, collect, [`/normalize.*`])
   .option(VERBOSE_KEYS.join(`, `), `verbose output`)
